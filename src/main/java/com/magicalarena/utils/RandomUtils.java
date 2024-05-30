62f6524a2f02f932da6a8523260779e049386a96
Yuvraj-2060 <yuvrajthakur2060@gmail.com>
2024-05-30 21:15:03 +0530

    Created a RandomUtil package for generating random integer value.

--- src/main/java/com/magicalarena/utils/RandomUtils.java
+++ src/main/java/com/magicalarena/utils/RandomUtils.java
@@ -4,6 +4,6 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class RandomUtils {
     public static int randomInt(int min, int max) {
-        return ThreadLocalRandom.current().nextInt(min, max + 1);
+         return ThreadLocalRandom.current().nextInt(min, max + 1);
     }
 }
